65f320db5f727db6178440ca220836728d6f0217
Antonio Anon Brasolin <tonhoanhon@gmail.com>
2022-07-29 08:12:56 +0900

    Add current turn indicator and current turn logic

--- World/TurnOrder/TurnOrder.gd
+++ World/TurnOrder/TurnOrder.gd
@@ -1,7 +1,9 @@
 extends Control
 
-onready var character_list_ui = $MarginContainer/CharacterList
+onready var character_list_ui = $ListArea/CharacterList
+onready var current_turn_indicator = $CurrentTurnIndicator
 var character_order := []
+var current_turn: CharacterNode = null
 
 func _ready():
 	for character_node in get_tree().get_nodes_in_group("Characters"):
@@ -9,6 +11,7 @@ func _ready():
 			character_order.append(character_node)
 
 	character_order.sort_custom(SpeedSorter, "sort_speed")
+	current_turn = character_order[0]
 	update_ui()
 
 func update_ui():
@@ -19,7 +22,21 @@ func update_ui():
 		var label = Label.new()
 		label.text = character_node.character.characterName
 		character_list_ui.add_child(label)
+	
+	update_turn_ui()
+
+func update_turn_ui():
+	if current_turn:
+		current_turn_indicator.global_position = current_turn.global_position
+		current_turn_indicator.visible = true
+	else:
+		current_turn_indicator.visible = false
 
 class SpeedSorter:
 	static func sort_speed(a: CharacterNode, b: CharacterNode):
 		return a.character.speed > b.character.speed
+
+func _on_NextTurnButton_pressed():
+	var idx = (character_order.find(current_turn) + 1) % character_order.size()
+	current_turn = character_order[idx]
+	update_turn_ui()
