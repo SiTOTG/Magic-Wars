d03ec66b37d9ef15578c0b0b23651d53f68979e8
Antonio Anon Brasolin <tonhoanhon@gmail.com>
2022-07-30 07:53:27 +0900

    Add defend and FSM to the main

--- Character/CharacterNode.gd
+++ Character/CharacterNode.gd
@@ -2,15 +2,21 @@ class_name CharacterNode
 extends Sprite
 
 export (Resource) var character = preload("res://Character/Dummy/Dummy.tres") as Character
+export (bool) var visible_name: bool = true setget set_name_visibility
 
 onready var animation_player = $AnimationPlayer
 onready var hp = character.max_hp setget set_hp
 onready var hp_label = $HP
+onready var name_label = $Name
 
 signal death
 
+var is_defending = false
+
 func _ready():
 	hp_label.text = str(hp)
+	name_label.visible = visible_name
+	name_label.text = character.characterName
 
 func set_hp(value):
 	hp = int(clamp(value, 0, character.max_hp))
@@ -21,4 +27,15 @@ func set_hp(value):
 
 
 func attack(target: CharacterNode):
-	target.hp -= character.damage
+	target.do_damage(character.damage)
+
+func do_damage(damage: int):
+	if not is_defending:
+		self.hp -= damage
+
+func set_name_visibility(value):
+	visible_name = value
+	name_label.visible = value
+
+func start_turn():
+	is_defending = false
--- Main.gd
+++ Main.gd
@@ -5,18 +5,57 @@ extends Node2D
 # var a = 2
 # var b = "text"
 onready var turn_order = $CanvasLayer/TurnOrder
+onready var defend_button = $CanvasLayer/MarginContainer/VBoxContainer/Defend
+
+enum Action{
+	Attack,
+	Defense,
+	Magic
+}
 
 var temp_buttons = []
+var status = Action.Attack setget set_status
 
 func _ready():
 	for character in get_tree().get_nodes_in_group("Characters"):
 		if character is CharacterNode:
 			character.connect("death", self, "_check_gameover_condition")
+	force_apply_status()
 
-func _on_NextTurn_pressed():
-	next_turn()
+func set_status(value):
+	var old_status = status
+	status = value
+	if status != old_status:
+		match old_status:
+			Action.Attack:
+				clear_temp_buttons()
+			Action.Defense:
+				defend_button.text = "Defend"
+
+		force_apply_status()
+
+func force_apply_status():
+	match status:
+		Action.Defense:
+			defend_button.text = "Confirm Defend"
+		Action.Attack:
+			attack_status()
+
+func defend_status():
+	status = Action.Defense
+	defend_button.text = "Confirm Defend"
+
+func _on_Defend_pressed():
+	if status == Action.Defense:
+		turn_order.current_turn.is_defending = true
+		next_turn()
+		return
+	self.status = Action.Defense
 
 func _on_Attack_pressed():
+	self.status = Action.Attack
+
+func attack_status():
 	for character in get_tree().get_nodes_in_group("Characters"):
 		if character is CharacterNode and character != turn_order.current_turn:
 			var button = Button.new()
@@ -28,11 +67,17 @@ func _on_Attack_pressed():
 			button.connect("pressed", self, "next_turn")
 			temp_buttons.append(button)
 
-func next_turn():
+func clear_temp_buttons():
 	for button in temp_buttons:
 		button.queue_free()
 	temp_buttons.clear()
+
+func next_turn():
+	clear_temp_buttons()
 	turn_order.go_to_next_turn()
+	turn_order.current_turn.start_turn()
+	self.status = Action.Attack
+	force_apply_status()
 
 func _check_gameover_condition():
 	yield(get_tree(),"idle_frame")
