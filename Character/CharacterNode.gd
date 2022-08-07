2d9a0e78f00d436030c4d343a0e7eafb11d0df3b
Antonio Anon Brasolin <tonhoanhon@gmail.com>
2022-08-07 09:09:37 +0900

    Add character slot to improve UI and HP bar

--- Character/CharacterNode.gd
+++ Character/CharacterNode.gd
@@ -2,25 +2,18 @@ class_name CharacterNode
 extends Sprite
 
 export (Resource) var character = preload("res://Character/Dummy/Dummy.tres") as Character
-export (bool) var visible_name: bool = true setget set_name_visibility
 
 onready var animation_player = $AnimationPlayer
 onready var hp = character.max_hp setget set_hp
-onready var hp_label = $HP
-onready var name_label = $Name
 
 signal death
+signal hp_updated(curhp, maxhp)
 
 var is_defending = false
 
-func _ready():
-	hp_label.text = str(hp)
-	name_label.visible = visible_name
-	name_label.text = character.characterName
-
 func set_hp(value):
 	hp = int(clamp(value, 0, character.max_hp))
-	hp_label.text = str(hp)
+	emit_signal("hp_updated", hp, character.max_hp)
 	if hp == 0:
 		emit_signal("death")
 		queue_free()
@@ -32,10 +25,6 @@ func attack(target: CharacterNode):
 func do_damage(damage: int):
 	if not is_defending:
 		self.hp -= damage
-
-func set_name_visibility(value):
-	visible_name = value
-	name_label.visible = value
 
 func start_turn():
 	is_defending = false
---
+++ Character/CharacterSlot.gd
@@ -0,0 +1,50 @@
+tool
+extends Control
+
+var character: CharacterNode = null
+var hpbar: TextureRect
+var current_hpbar: TextureRect
+var nameLabel: Label
+
+func _ready():
+	init_ready()
+
+func init_ready():
+	hpbar = $hpbar
+	current_hpbar = $hpbar/current
+	
+	for child in get_children():
+		if child is CharacterNode:
+			init_character(child)
+			break
+
+
+func init_character(child: CharacterNode):
+	character = child
+	character.centered = false
+	character.connect("hp_updated", self, "_on_hp_updated")
+	nameLabel = $NameLabel
+	nameLabel.text = character.character.characterName
+	update_hp_bar(character.hp, character.character.max_hp)
+
+func _on_hp_updated(hp, max_hp):
+	update_hp_bar(hp, max_hp)
+
+func update_hp_bar(hp, max_hp):
+	var hp_rate = 1.0*hp/max_hp
+	var tween = get_tree().create_tween()
+	var before = current_hpbar.rect_size
+	var end = Vector2(before)
+	end.x = int(hp_rate*hpbar.rect_size.x)
+	tween.tween_property(current_hpbar, "rect_size", end, 0.6)
+
+#	current_hpbar.rect_size.x = int(hp_rate*hpbar.rect_size.x)
+
+func _get_configuration_warning():
+	for child in get_children():
+		if child is CharacterNode:
+			if child.centered:
+				return "Remove centered in order to fit in the UI"
+			return ""
+	return "Needs to have a CharacterNode to work."
+
