332e850bceb076a5e323ef75b7119aa261584188
Antonio Anon Brasolin <tonhoanhon@gmail.com>
2022-08-13 01:49:55 +0900

    Add basic fireball

--- Actions/Spells/Fireball/Fireball.gd
+++ Actions/Spells/Fireball/Fireball.gd
@@ -1,5 +1,7 @@
 extends SpellAction
 
+const Fireball = preload("res://Assets/spells/Fireball/FireballEffect.tscn")
+
 func do_apply_action(targets):
 	.do_apply_action(targets)
 	var origin = turn_order.current_turn as CharacterNode
@@ -7,5 +9,13 @@ func do_apply_action(targets):
 	origin.animation_player.queue("Attack")
 	origin.animation_player.queue("Idle")
 	for character in targets:
+		var fireball = Fireball.instance()
+		fireball.play("Flying")
+		fireball.global_position = origin.global_position
+		EffectPlayer.add_child(fireball)
+		fireball.flip_h = character.global_position.x < origin.global_position.x
+		var tween = GlobalTree.get_tree().create_tween()
+		tween.tween_property(fireball, "global_position", character.global_position + character.get_rect().size/2, 0.6)
+		tween.tween_callback(fireball, "play", ["Impact"])
+		fireball.connect("animation_finished", fireball, "queue_free")
 		origin.attack(character)
-	pass
