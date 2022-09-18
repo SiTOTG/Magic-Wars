3cfe605520d7fd80314de3f78e33d995b9e067b1
Antonio Anon Brasolin <tonhoanhon@gmail.com>
2022-09-19 07:04:44 +0900

    Create wireframe for pre battle screen with party

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,3 +4,4 @@ extends Resource
 export (Array, Resource) var characters = []
 export (Array, Resource) var item_slots = []
 export (String, "P1 Characters", "P2 Characters") var player_group = "P1 Characters" 
+export (int, 20, 100) var buy_points = 50
---
+++ World/Battle/PreBattle.gd
@@ -0,0 +1,10 @@
+extends Control
+
+var player: Player
+
+func _ready():
+	# Default, just in case it wasn't provided
+	player = preload("res://Player/P1.tres")
+
+func setup():
+	pass
