3c1dad287bc88324a4267dc9d6e66770a2c5bc04
Antonio Anon Brasolin <tonhoanhon@gmail.com>
2022-09-19 08:56:22 +0900

    Add single character view

--- Character/Character.gd
+++ Character/Character.gd
@@ -6,3 +6,4 @@ export (String) var characterName := "Joao"
 export (int, 1, 10000) var max_hp = 5
 export (int, 0, 100) var damage = 1
 export (int, 0, 1000) var max_mp = 100 
+export (int, 3, 100) var point_cost = 3
