1763c682a11db4568e34102b3bc769e56fc77fb5
Antonio Anon Brasolin <tonhoanhon@gmail.com>
2022-09-21 06:52:47 +0900

    Add basis for character preview on pre battle screen

--- Character/Character.gd
+++ Character/Character.gd
@@ -7,3 +7,4 @@ export (int, 1, 10000) var max_hp = 5
 export (int, 0, 100) var damage = 1
 export (int, 0, 1000) var max_mp = 100 
 export (int, 3, 100) var point_cost = 3
+export (Texture) var icon = preload("res://icon.png")
---
+++ World/Battle/CharacterPreview.gd
@@ -0,0 +1,17 @@
+extends HBoxContainer
+
+func update_preview(characters):
+	for i in range(4):
+		var textureRect: TextureRect = get_children()[i].get_node("TextureRect")
+		if i < characters.size():
+			var character: Character = characters[i]
+			textureRect.texture = character.icon
+		else:
+			textureRect.texture = null
+
+func _on_SingleCharacterView_character_selected(character: Character, current_points, all_selected_characters):
+	update_preview(all_selected_characters)
+
+
+func _on_SingleCharacterView_character_unselected(character, current_points, all_selected_characters):
+	update_preview(all_selected_characters)
