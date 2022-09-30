4fb764a8b98acdc8f73ce657a7fb7060568e1885
Antonio Anon Brasolin <tonhoanhon@gmail.com>
2022-10-01 08:45:03 +0900

    Add music and settings

--- World/MainMenu/MainMenu.gd
+++ World/MainMenu/MainMenu.gd
@@ -1,6 +1,10 @@
 extends Control
 
+onready var settingsScreen := $Settings
 
-
-func _on_Button_pressed():
+func _on_StartButton_pressed():
 	get_tree().change_scene("res://World/Battle/PreBattle/PreBattle.tscn")
+
+
+func _on_SettingsButton_pressed():
+	settingsScreen.visible = true
